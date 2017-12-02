9babb81de432e2f3211bd1c9026f25618b9f96c5
alexandre-martins <alexandre-martins@PRO-PAE-7005>
2017-12-02 08:27:14 -0200

    teste

---
+++ mantrares/src/main/java/mantrares/com/br/mantrares/App.java
@@ -0,0 +1,13 @@
+package mantrares.com.br.mantrares;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
