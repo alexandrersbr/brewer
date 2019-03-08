d924d62ec2bb40b63fd3e3b91fa3b5c8d5f9f5b7
alexandre-martins <alexandre-martins@PRO-PAE-7005>
2019-03-08 08:21:03 -0300

    uPDATE 8/3

--- brewer/src/main/java/com/algaworks/brewer/controller/ClientesController.java
+++ brewer/src/main/java/com/algaworks/brewer/controller/ClientesController.java
@@ -1,35 +1,26 @@
 package com.algaworks.brewer.controller;
 
-import javax.validation.Valid;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.ModelAndView;
 
-import com.algaworks.brewer.model.Cliente;
+import com.algaworks.brewer.model.TipoPessoa;
+import com.algaworks.brewer.repository.Estados;
 
 @Controller
+@RequestMapping("/clientes")
 public class ClientesController {
 
-	@RequestMapping("/clientes/novo")
-	public String novo(Cliente cliente) {
-		return "cliente/CadastroCliente";
-	}
+	@Autowired
+	private Estados estados;
 	
-	@RequestMapping(value = "/clientes/novo", method = RequestMethod.POST)
-	public String cadastrar(@Valid Cliente cliente, BindingResult result, Model model, RedirectAttributes attributes) {
-		if (result.hasErrors()) {
-			return novo(cliente);
-		}
-		
-		// Salvar no banco de dados...
-	//	attributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso!");
-//		System.out.println(">>> sku: " + cerveja.getSku());
-		return "redirect:/cervejas/novo";
+	@RequestMapping("novo")
+	public ModelAndView novo() {
+		ModelAndView mv = new ModelAndView("cliente/CadastroCliente");
+		mv.addObject("tiposPessoa", TipoPessoa.values());
+		mv.addObject("estados", estados.findAll());
+		return mv;
 	}
-	
 	
 }
